f2855bb8b2b9f4bdbe7b07b017df31a74c275f1e
Ryuhei Yoshida <yoshida.ryuhei0112@gmail.com>
2025-01-08 01:06:23 +0900

    Fix install_dep_packages

--- install_dep_packages.sh
+++ install_dep_packages.sh
@@ -4,7 +4,7 @@
 rye sync
 #pip3 install -Iv kubernetes==11.0.0
 #pip3 install -Iv openshift==0.11.2
-ansible-galaxy collection install kubernetes.core
+rye run ansible-galaxy collection install kubernetes.core
 
 #pip3 install -Iv jinja2==3.0.3
 #pip3 install jinja2-base64-filters==0.1.4
